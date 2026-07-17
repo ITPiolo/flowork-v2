ab1191a0ada687da33b8370f0b9a710f6fe18729
Piolo Dungca <piolodungca18@gmail.com>
2026-07-17 09:27:36 +0400

    Fix sticky positioning broken by page transition transform

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -8,8 +8,8 @@ export default function PageTransition({ children }: { children: React.ReactNode
   return (
     <motion.div
       key={pathname}
-      initial={{ opacity: 0, y: 8 }}
-      animate={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       transition={{ duration: 0.35, ease: [0.22, 1, 0.36, 1] }}
     >
       {children}
